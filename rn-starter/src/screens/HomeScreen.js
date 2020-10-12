e7fba4a13ec515368a71ff07c564f96ae38f664d
lukelin1991 <lukelin1991@yahoo.com>
2020-10-12 12:04:30 -0400

    creating a button on native app

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Chicken Tenderz</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Chicken Tenderz</Text>
+      <Button 
+        onPress={() => console.log('Button pressed')}
+        title="Go to Components Demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
