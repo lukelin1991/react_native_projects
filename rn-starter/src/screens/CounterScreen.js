00cdf3d02cd55e45a20b54e2fbcb03469eb6cc21
lukelin1991 <lukelin1991@yahoo.com>
2020-10-13 12:32:50 -0400

    react

---
+++ rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const CounterScreen = () => {
+    // todo: fix this
+    let counter = 0
+    return (
+        <View>
+            <Button title="Increase" onPress={() => {
+                counter++
+            }} />
+            <Button title="Decrease" onPress={() => {
+                counter--
+            }}/>
+            <Text>Current Count:{counter}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default CounterScreen
