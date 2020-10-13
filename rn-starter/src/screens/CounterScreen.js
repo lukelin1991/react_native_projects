58cdb45306dbb72f2a57100834d518ac05c5ec78
lukelin1991 <lukelin1991@yahoo.com>
2020-10-13 12:35:45 -0400

    react hooks with counter

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 const CounterScreen = () => {
-    // todo: fix this
-    let counter = 0
+    const [counter, setCounter] = useState(0)
+
     return (
         <View>
             <Button title="Increase" onPress={() => {
-                counter++
+                setCounter(counter + 1)
             }} />
             <Button title="Decrease" onPress={() => {
-                counter--
+                setCounter(counter - 1)
             }}/>
             <Text>Current Count:{counter}</Text>
         </View>
