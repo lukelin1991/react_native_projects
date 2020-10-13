371d8fc2e7e4d8fef2d9af16d8785a12a4e1730f
lukelin1991 <lukelin1991@yahoo.com>
2020-10-12 21:20:29 -0400

    created image

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -5,10 +5,9 @@ import ImageDetail from '../components/ImageDetail'
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title="Forest"/>
-            <ImageDetail />
-            <ImageDetail />
-            <ImageDetail />
+            <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
+            <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} />
+            <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} />
         </View>
     )
 }
